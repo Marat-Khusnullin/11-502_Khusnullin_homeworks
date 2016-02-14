aa47b54435397f34aa9ec01dac140fbc2164f7cb
Marat Khusnullin <55klevanso55@gmail.com>
2016-02-15 01:56:43 +0300

    origin

---
+++ Farey/src/ru/itis/inform/Main.java
@@ -0,0 +1,11 @@
+package ru.itis.inform;
+
+public class Main {
+
+    public static void main(String[] args) {
+        FareyGenerate Z = new FareyGenerate();
+        Z.Generate(7);
+        Z.Print();
+    }
+    }
+
---
+++ Farey/src/ru/itis/inform/RationalNumber.java
@@ -0,0 +1,30 @@
+package ru.itis.inform;
+
+/**
+ * Created by Марат on 14.02.2016.
+ */
+public class RationalNumber {
+    private int a;
+    private int b;
+
+    public RationalNumber() {
+        this.a = 0;
+        this.b = 1;
+    }
+
+    public RationalNumber(int a, int b) {
+        this.a = a;
+        if (b == 0) {
+            throw new IllegalArgumentException();
+        } else this.b = b;
+    }
+
+
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+}
